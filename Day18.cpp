123f24ca2923aae72d9bb3dedffa0365a108bf05
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-03 15:23:38 +0530

    Create Day18.cpp

---
+++ Day18.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int num;
+    cin>>num;
+    if(num >= 90){
+        printf("A");}
+    else if(num >= 80){
+        printf("B");}
+    else if(num >= 70){
+        printf("C");}
+    else if(num >= 60){
+        printf("D");}
+    else if(num >= 50){
+        printf("E");}
+    else{
+        printf("F");}
+    // Your code goes here
+    
+    return 0;
+}
