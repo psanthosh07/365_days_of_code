683a94e13d2466c2de9a4b3a9265ca49b1f4dd2a
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-03 15:01:32 +0530

    Create Day12.py

---
+++ Day12.py
@@ -0,0 +1,10 @@
+def main():
+    names = []
+    names.append('Robin')
+    names.append('Aman')
+    names.append('Rahul')
+    print(names)
+    return 0
+
+if __name__ == '__main__':
+    main()
