b90f558bcbbec928eddc78c41dccb83564b6f3ce
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-03 14:30:32 +0530

    Create Day6.py

---
+++ Day6.py
@@ -0,0 +1,11 @@
+def main():
+    N = int(input())
+    for i in range(1,N):
+        if i%2!=0:
+            print(i)
+    # YOUR CODE GOES HERE
+
+    return 0
+
+if __name__ == '__main__':
+    main()
