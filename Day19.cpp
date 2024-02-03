2e36331da44645a1aedfceeddaf68429640e3cc0
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-03 15:30:51 +0530

    Create Day19.cpp

---
+++ Day19.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int x, y;
+    cin>>x>>y;
+    if(x<=y){
+    printf("Robin");}
+    else{
+    printf("Rahul");}
+    // YOUR CODE GOES HERE
+    
+    return 0;
+}
