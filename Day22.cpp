facae73a5053fdf09c7e41fdf006506adabdfdab
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-03 15:50:54 +0530

    Update Day22.cpp

--- Day22.cpp
+++ Day22.cpp
@@ -3,33 +3,12 @@
 using namespace std;
 
 int main()  {
-    int weekday;
-    cin>>weekday;
-    switch(weekday){
-        case 1:
-        cout<<"Monday";
-        break;
-        case 2:
-        cout<<"Tuesday";
-        break;
-        case 3:
-        cout<<"Wednesday";
-        break;
-        case 4:
-        cout<<"Thursday";
-        break;
-        case 5:
-        cout<<"Friday";
-        break;
-        case 6:
-        cout<<"Saturday";
-        break;
-        case 7:
-        cout<<"Sunday";
-        break;
-        
-        
-    }
+    int x, y;
+    cin>>x>>y;
+    if(x<=y){
+    printf("Robin");}
+    else{
+    printf("Rahul");}
     // YOUR CODE GOES HERE
     
     return 0;
