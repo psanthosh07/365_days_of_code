4d128223bf57e2c79a681400351bbbea86237b95
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-03 15:55:12 +0530

    Create Day23.cpp

---
+++ Day23.cpp
@@ -0,0 +1,23 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// Your Code goes here
+
+int compute(int A,int B){
+    int n=(A*A)+(B*B);
+    return n;
+}
+
+
+
+
+/*
+int main()  {
+    int A, B;
+    cin>>A>>B;
+    cout<<compute(A, B)<<endl;
+    return 0;
+}
+*/
