d9d64f26d2083f5d47a45e3d8cad09642afa5ff6
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-09 21:20:17 +0530

    Create Day 24.cpp

---
+++ Day 24.cpp
@@ -0,0 +1,22 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+
+int compute(int A,int B){
+    int n=(A*A)+(B*B);
+    return n;
+}
+
+
+
+
+/*
+int main()  {
+    int A, B;
+    cin>>A>>B;
+    cout<<compute(A, B)<<endl;
+    return 0;
+}
+*/
