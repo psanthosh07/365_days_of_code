eed7a6a30547b2a4f6d68f54707a3b969f082123
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-10 20:20:23 +0530

    Create Day40.cpp

---
+++ Day40.cpp
@@ -0,0 +1,27 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// YOUR CODE GOES HERE
+int compute(int num1, int num2) {
+    return abs(num1 - num2);
+}
+
+// Function to compute square of an integer
+int compute(int num) {
+    return num * num;
+}
+
+
+/*
+int main()  {
+    int x,y;
+    cin>>x>>y;
+    cout<<compute(x,y)<<endl;
+    int z;
+    cin>>z;
+    cout<<compute(z)<<endl;
+    return 0;
+}
+*/
