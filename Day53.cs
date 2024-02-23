ecf7cd7001a814e1a5ea181873ab479b91d7c0fb
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-23 18:25:36 +0530

    Create Day53.cs

---
+++ Day53.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+class MAIN  {
+    public static void Main(string[] args) {
+        System.Console.WriteLine("Hello, InterviewBit!");
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUT
+        // E.g. 'StreamReader' for input & 'StreamWriter' for output
+        
+    }
+}
