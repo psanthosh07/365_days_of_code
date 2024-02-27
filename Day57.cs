9eb9fedf355659222db10b3f68c59e307e0d4abc
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-02-27 23:08:14 +0530

    Create Day57.cs

---
+++ Day57.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+
+class MAIN {
+    public static void Main(string[] args) {
+        // Take input from standard input (stdin)
+        string input = Console.ReadLine();
+
+        // Print output to standard output (stdout)
+        Console.WriteLine("Hello " + input);
+    }
+}
