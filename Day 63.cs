7797ba5de265126e80e461b86ef81446f2b97eae
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2024-03-04 20:10:28 +0530

    Update Day 63.cs

--- Day 63.cs
+++ Day 63.cs
@@ -1,1 +1,18 @@
+using System;
+
+class MAIN  
+{
+    public static int addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static void Main(string[] args) 
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int sum = addition(a, b);
+        Console.WriteLine(sum);
+    }
+}
 
