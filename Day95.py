fda3760e1d15b0df9a6e1ed076bd92403476c1fa
Santhosh P <58479203+psanthosh07@users.noreply.github.com>
2024-04-05 21:44:10 +0530

    Create Day95.py

---
+++ Day95.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param A : string
+    # @return a strings
+    def solve(self, A):
+        l=A.split()
+        s=l[::-1]
+        s=' '.join(s)
+        
+
+        return s
