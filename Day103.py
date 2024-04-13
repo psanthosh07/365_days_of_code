a8b3ba21f50774b1ce6e781b51d853ad1013c342
Santhosh P <58479203+psanthosh07@users.noreply.github.com>
2024-04-13 18:29:16 +0530

    Create Day103.py

---
+++ Day103.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    import math
+    def solve(self, A, B):
+        c=(B/A)
+        n=math.ceil(c/5)
+        return n
+        
+        
+        
