70e2279fcbd08fc2420293a5e27d93758c8d1404
Santhosh P <58479203+psanthosh07@users.noreply.github.com>
2024-06-13 22:56:38 +0530

    Create Day164.sh

---
+++ Day164.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Incase you have to take input, please take it from file named 'input' (without quotes) [e.g. cat input]
+# Print your final output to console. Do not redirect to another file.
+# E.g. Suppose the question is to print the content of a file.
+#      Your solution should be 'cat input'(without quotes) instead of 'cat input > output'. That's it!
+# Your code starts from here...
+
+# Using grep to match the valid phone number patterns from the input file
+grep -E '^(\([0-9]{3}\) [0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4})$' input
