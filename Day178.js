76bf629978753555dc3d6d45c6c32655276b342c
Santhosh P <58479203+psanthosh07@users.noreply.github.com>
2024-06-29 21:40:37 +0530

    Create Day178.js

---
+++ Day178.js
@@ -0,0 +1,7 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var N = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+var log2Value = Math.round(Math.log2(N));
+
+console.log(log2Value);
