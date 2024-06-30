c1ee1f12de0b2cc69e5447e65d80d05bc316625d
Santhosh P <58479203+psanthosh07@users.noreply.github.com>
2024-06-30 23:52:13 +0530

    Create Day179.js

---
+++ Day179.js
@@ -0,0 +1,8 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num value is in variable *num*
+var S = readLine().trim();
+var lengthOfS = S.length;
+
+var result = S + lengthOfS;
+
+console.log(result);
