23fe361653bba09d1fb19f15bda107bc159b4e7f
Santhosh P <58479203+psanthosh07@users.noreply.github.com>
2024-07-01 23:37:37 +0530

    Create Day180.js

---
+++ Day180.js
@@ -0,0 +1,9 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num value is in variable *num*
+var S = readLine().trim();
+// YOUR CODE GOES HERE
+let upperCaseString = S.toUpperCase();
+
+let result = S + upperCaseString;
+
+console.log(result);
